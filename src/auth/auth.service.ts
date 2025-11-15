86b0705550221127f49aca5422107c3f65c9cf1d
wilmerx5 <wilmercampos2004@gmail.com>
2025-11-14 22:58:28 -0500

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -147,7 +147,7 @@ export class AuthService {
 
     const token = Math.floor(100000 + Math.random() * 900000).toString();
 
-    const expiresAt = new Date(Date.now() + 2 * 60 * 1000);
+    const expiresAt = new Date(Date.now() + 20 * 60 * 1000);
 
     const newToken = this.verificationTokenRepository.create({
       token,
