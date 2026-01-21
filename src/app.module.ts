c95f2a0f82e0ecda2ab2646644de547d2ddeedc6
wilmerx5 <wilmercampos2004@gmail.com>
2026-01-20 22:32:32 -0500

    final

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import { PaymentsModule } from './payments/payments.module';
           password: configService.get<string>('DB_PASSWORD'),
           database: configService.get<string>('DB_DATABASE'),
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: true,
+          synchronize: true, // Keep true to sync schema with dev - will disable later and use migrations
           // Store all dates in UTC - conversion to Bogotá timezone happens in application layer
           timezone: 'Z', // UTC timezone
         };
